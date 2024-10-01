86991fb41417648792fc261060f0459de005eb46
Osvaldo <osvaldovergar02@gmail.com>
2024-10-01 17:27:03 -0400

    Primer commit - Añadir proyecto Java

---
+++ MiInformacion.java
@@ -0,0 +1,15 @@
+public class MiInformacion { 
+    public static void main(String[] args) { 
+        // Definir las variables 
+        String nombre = "Osvaldo Vergara"; 
+        int edad = 32; String pais = "Paraguay"; 
+        String ciudad = "Asuncion"; 
+        String gustos = "jugar futbol"; 
+        // Imprimir la información utilizando las variables 
+        System.out.println("Hola, mi nombre es " + nombre + "."); 
+        System.out.println("Tengo " + edad + " años de edad."); 
+        System.out.println("Soy de " + pais + " y vivo en la ciudad de " + ciudad + "."); 
+        System.out.println("Me gusta mucho " + gustos + ".");
+     } 
+    
+    }
